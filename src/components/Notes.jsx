30fe60f975ac7cc1477b595a67b11845da830149
Ravi Kant Rohilla <108731971+coderush84@users.noreply.github.com>
2022-08-21 15:15:14 +0530

    Create Notes.jsx

---
+++ src/components/Notes.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>My first Hike on everest</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae aut,
+        qui ullam nobis quis expedita consequatur debitis hic dicta asperiores
+        vitae odit omnis totam suscipit officia, repudiandae illo emet!
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
